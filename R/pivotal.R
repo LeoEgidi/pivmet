41b94d46b212b3bae65c1c2b44db74529db129b7
Leonardo Egidi <legidi@units.it>
2019-06-19 09:47:16 +0200

    Update pivotal.R

--- R/pivotal.R
+++ R/pivotal.R
@@ -1,8 +1,10 @@
 #'Pivotal Selection via Co-Association Matrix
 #'
 #'
-#'Finding the pivots according to three different
-#'methods involving a co-association matrix C.
+#'Finding pivotal units from a data partition and a
+#'co-association matrix C
+#'according to three different methods.
+#'
 #'@param C A \eqn{N \times N} co-association matrix, i.e.
 #'a matrix whose elements are co-occurrences of pair of units
 #'in the same cluster among \eqn{H} distinct partitions.
@@ -14,7 +16,7 @@
 #'@details
 #'
 #' Given a set of \eqn{N} observations \eqn{(y_{1},y_{2},...,y_{N})}
-#' (\eqn{y_i} may be a \eqn{d}-dimensional vector, \eqn{d \geq 1}),
+#' (\eqn{y_i} may be a \eqn{d}-dimensional vector, \eqn{d \ge 1}),
 #' consider clustering methods to obtain \eqn{H} distinct partitions
 #' into \eqn{k} groups.
 #' The matrix \code{C} is the co-association matrix,
