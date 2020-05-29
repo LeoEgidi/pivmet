a34e17b8ebbca35d1468bf89a0ffbc828c10b4e7
Leonardo Egidi <legidi@units.it>
2020-05-29 12:24:26 +0200

    Update Relabelling_in_Bayesian_mixtures_by_pivotal_units.R

--- vignettes/Relabelling_in_Bayesian_mixtures_by_pivotal_units.R
+++ vignettes/Relabelling_in_Bayesian_mixtures_by_pivotal_units.R
@@ -30,36 +30,3 @@ W   <- c(0.2,0.8)
 sim <- piv_sim(N = N, k = k, Mu = Mu,
  Sigma.p1 = Sigma.p1, Sigma.p2 = Sigma.p2, W = W)
 
-## ----mcmc, message =  FALSE, warning = FALSE-----------------------------
-res <- piv_MCMC(y = sim$y, k= k, nMC =nMC)
-
-## ----pivotal_rel, fig.show='hold', fig.align='center',fig.width=7--------
-rel <- piv_rel(mcmc=res)
-piv_plot(y = sim$y, mcmc = res, rel_est = rel, par = "mean", type = "chains")
-piv_plot(y = sim$y, mcmc = res, rel_est = rel, type = "hist")
-
-## ----fish_hist, fig.align ='center', fig.width=5.5-----------------------
-data(fish)
-y <- fish[,1]
-hist(y, breaks=40, prob = TRUE, cex.lab=1.6,
-             main ="Fishery data", cex.main =1.7,
-             col="navajowhite1", border="navajowhite1")
- lines(density(y), lty=1, lwd=3, col="blue")
-
-## ----fish_data-----------------------------------------------------------
-k <- 5
-nMC <- 15000
-res <- piv_MCMC(y = y, k = k, nMC = nMC, burn = 0.5*nMC,
-software = "rjags")
-
-## ----true_iter-----------------------------------------------------------
-res$true.iter
-
-## ----fish_rel, fig.align= 'center', fig.width=7--------------------------
-rel <- piv_rel(mcmc=res)
-piv_plot(y=y, res, rel, par = "mean", type="chains")
-piv_plot(y=y, res, rel, type="hist")
-
-## ----model_code----------------------------------------------------------
-cat(res$model)
-
