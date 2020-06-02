82a0d8e24b94cf3d9a67e955e9d20c87d94e787c
LeoEgidi <leoegidi@hotmail.it>
2020-06-02 12:46:41 +0200

    ff

--- vignettes/Relabelling_in_Bayesian_mixtures_by_pivotal_units.R
+++ vignettes/Relabelling_in_Bayesian_mixtures_by_pivotal_units.R
@@ -29,3 +29,76 @@ W   <- c(0.2,0.8)
 sim <- piv_sim(N = N, k = k, Mu = Mu,
  Sigma.p1 = Sigma.p1, Sigma.p2 = Sigma.p2, W = W)
 
+## ----fish_data----------------------------------------------------------------
+k <- 5
+nMC <- 15000
+res <- piv_MCMC(y = y, k = k, nMC = nMC, 
+                burn = 0.5*nMC, software = "rjags")
+
+## ----true_iter----------------------------------------------------------------
+res$true.iter
+
+## ----fish_rel, fig.align= 'center', fig.width=7-------------------------------
+rel <- piv_rel(mcmc=res)
+piv_plot(y=y, res, rel, par = "mean", type="chains")
+piv_plot(y=y, res, rel, type="hist")
+
+## ----mcmc, message =  FALSE, warning = FALSE----------------------------------
+res <- piv_MCMC(y = sim$y, k= k, nMC =nMC, 
+                piv.criterion = "MUS")
+
+## ----pivotal_rel, fig.show='hold', fig.align='center',fig.width=7-------------
+rel <- piv_rel(mcmc=res)
+piv_plot(y = sim$y, mcmc = res, rel_est = rel, par = "mean", type = "chains")
+piv_plot(y = sim$y, mcmc = res, rel_est = rel, type = "hist")
+
+## ----fish_hist, fig.align ='center', fig.width=5.5----------------------------
+data(fish)
+y <- fish[,1]
+hist(y, breaks=40, prob = TRUE, cex.lab=1.6,
+             main ="Fishery data", cex.main =1.7,
+             col="navajowhite1", border="navajowhite1")
+ lines(density(y), lty=1, lwd=3, col="blue")
+
+## ----fish_data----------------------------------------------------------------
+k <- 5
+nMC <- 15000
+res <- piv_MCMC(y = y, k = k, nMC = nMC, 
+                burn = 0.5*nMC, software = "rjags")
+
+## ----true_iter----------------------------------------------------------------
+res$true.iter
+
+## ----fish_rel, fig.align= 'center', fig.width=7-------------------------------
+rel <- piv_rel(mcmc=res)
+piv_plot(y=y, res, rel, par = "mean", type="chains")
+piv_plot(y=y, res, rel, type="hist")
+
+## ----stan, fig.align= 'center', fig.width=7-----------------------------------
+res2 <- piv_MCMC(y = y, k = k, nMC = 3000, 
+                 software = "rstan")
+rel2 <- piv_rel(res2)
+piv_plot(y=y, res2, rel2, par = "mean", type="chains")
+
+## ----bayesplot,fig.align= 'center', fig.width=5-------------------------------
+posterior <- as.array(res2$stanfit)
+mcmc_intervals(posterior, regex_pars = c("mu"))
+
+## ----model_code---------------------------------------------------------------
+cat(res$model)
+cat(res2$model)
+
+## ----stan, fig.align= 'center', fig.width=7-----------------------------------
+res2 <- piv_MCMC(y = y, k = k, nMC = 3000, 
+                 software = "rstan")
+rel2 <- piv_rel(res2)
+piv_plot(y=y, res2, rel2, par = "mean", type="chains")
+
+## ----bayesplot,fig.align= 'center', fig.width=5-------------------------------
+posterior <- as.array(res2$stanfit)
+mcmc_intervals(posterior, regex_pars = c("mu"))
+
+## ----model_code---------------------------------------------------------------
+cat(res$model)
+cat(res2$model)
+
