714f995c94ffbd8005b32ab64daf09ecdb08d3df
Leonardo Egidi <legidi@units.it>
2021-08-31 15:52:17 +0200

    Update sparse_simul.R

--- R/sparse_simul.R
+++ R/sparse_simul.R
@@ -15,7 +15,7 @@ W <- c(0.5,0.5)
 sim <- piv_sim(N = N, k = k, Mu = Mu,
                Sigma.p1 = Sigma.p1,
                Sigma.p2 = Sigma.p2, W = W)
-res <- piv_MCMC(y = sim$y, k = k_over, nMC = nMC,
+res_biv <- piv_MCMC(y = sim$y, k = k_over, nMC = nMC,
                 sparsity =TRUE,
                 priors = list(a_sp=2, b_sp=2))
 
@@ -30,28 +30,28 @@ k <- 8
 ## rjags
 res2 <- piv_MCMC(y, k, nMC, sparsity = TRUE,
                  priors = list(alpha = rep(0.01, k))) # sparse on eta
-pdf(file="sparse_001.pdf", height = 6, width = 8)
+pdf(file="sparse_001.pdf", height = 7, width = 8)
 barplot(table(res2$nclusters), xlab= expression(K["+"]),
         col = "blue", border = "red", main = expression(paste("p(",K["+"], "|y)")),
-        cex.main=2, yaxt ="n", cex.axis=1.6, cex.names=1.6,
+        cex.main=3, yaxt ="n", cex.axis=2.4, cex.names=2.4,
         cex.lab=2)
 dev.off()
 
 
 res <- piv_MCMC(y, k, nMC, sparsity = TRUE) # uniform on eta
-pdf(file="sparse_1.pdf", height = 6, width = 8)
+pdf(file="sparse_1.pdf", height = 7, width = 8)
 barplot(table(res$nclusters), xlab= expression(K["+"]),
         col = "blue", border = "red", main = expression(paste("p(",K["+"], "|y)")),
-        cex.main=2, yaxt ="n",cex.axis=1.6, cex.names=1.6,
+        cex.main=3, yaxt ="n",cex.axis=2.4, cex.names=2.4,
         cex.lab=2)
 dev.off()
 
 res3 <- piv_MCMC(y, k, nMC, sparsity = TRUE,
                  priors = list(alpha = rep(10, k))) # overfitting on eta
-pdf(file="sparse_10.pdf", height = 6, width = 8)
+pdf(file="sparse_10.pdf", height = 7, width = 8)
 barplot(table(res3$nclusters), xlab= expression(K["+"]),
         col = "blue", border = "red", main = expression(paste("p(",K["+"], "|y)")),
-        cex.main=2, yaxt ="n", cex.axis=1.6, cex.names=1.6,
+        cex.main=3, yaxt ="n", cex.axis=2.4, cex.names=2.4,
         cex.lab=2)
 dev.off()
 
