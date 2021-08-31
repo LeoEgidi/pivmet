3faf9ed3cf294c1cc41000e7f276e2dbbc29ec00
Leonardo Egidi <legidi@units.it>
2021-08-31 15:23:12 +0200

    Update sparse_simul.R

--- R/sparse_simul.R
+++ R/sparse_simul.R
@@ -30,19 +30,29 @@ k <- 8
 
 res2 <- piv_MCMC(y, k, nMC, sparsity = TRUE,
                  priors = list(alpha = rep(0.01, k))) # sparse on eta
+pdf(file="sparse_001.pdf", height = 6, width = 8)
 barplot(table(res2$nclusters), xlab= expression(K["+"]),
         col = "blue", border = "red", main = expression(paste("p(",K["+"], "|y)")),
-        cex.main=2, yaxt ="n")
+        cex.main=2, yaxt ="n", cex.axis=1.6, cex.names=1.6,
+        cex.lab=2)
+dev.off()
+
 
 res <- piv_MCMC(y, k, nMC, sparsity = TRUE) # uniform on eta
+pdf(file="sparse_1.pdf", height = 6, width = 8)
 barplot(table(res$nclusters), xlab= expression(K["+"]),
         col = "blue", border = "red", main = expression(paste("p(",K["+"], "|y)")),
-        cex.main=2, yaxt ="n")
+        cex.main=2, yaxt ="n",cex.axis=1.6, cex.names=1.6,
+        cex.lab=2)
+dev.off()
 
 res3 <- piv_MCMC(y, k, nMC, sparsity = TRUE,
                  priors = list(alpha = rep(10, k))) # overfitting on eta
+pdf(file="sparse_10.pdf", height = 6, width = 8)
 barplot(table(res3$nclusters), xlab= expression(K["+"]),
         col = "blue", border = "red", main = expression(paste("p(",K["+"], "|y)")),
-        cex.main=2, yaxt ="n")
+        cex.main=2, yaxt ="n", cex.axis=1.6, cex.names=1.6,
+        cex.lab=2)
+dev.off()
 
 
